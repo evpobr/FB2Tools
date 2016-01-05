76937cd76f28b01687dee200077adf291d5b505f
evpobr <evpobr@gmail.com>
2016-01-05 15:01:57 +0500

    Enable static linking to runtime library

--- FBShell/FBShell_h.h
+++ FBShell/FBShell_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Jan 05 14:26:44 2016
+/* at Tue Jan 05 14:41:37 2016
  */
 /* Compiler settings for FBShell.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
