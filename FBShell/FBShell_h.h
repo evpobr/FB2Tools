4e938c0ee8a1428477acd93218eb47096384cbb9
evpobr <evpobr@gmail.com>
2016-01-05 11:18:45 +0500

    Fix NuGet package restore

--- FBShell/FBShell_h.h
+++ FBShell/FBShell_h.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 8.00.0603 */
-/* at Tue Jan 05 10:50:16 2016
+/* at Tue Jan 05 11:16:53 2016
  */
 /* Compiler settings for FBShell.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0603 
